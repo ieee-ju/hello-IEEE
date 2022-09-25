5a169899a98b8f7a1c01b7386e566ac54580eb26
sainik-khaddar <sainikwarror132@gmail.com>
2022-09-25 12:17:26 +0530

    TIMER STOPPED

--- timer.js
+++ timer.js
@@ -10,12 +10,22 @@ const hours =  Math.floor(diff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60))
 const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
 const seconds = Math.floor((diff % (1000 * 60)) / 1000)
 
+if(days>0)
+{
       document.querySelector(".seconds").innerHTML = seconds < 10 ? '0' + seconds : seconds
       document.querySelector(".minutes").innerHTML = minutes < 10 ? '0' + minutes :minutes
       document.querySelector(".hours").innerHTML = hours < 10 ? '0' + hours : hours
       document.querySelector(".days").innerHTML = days < 10 ? '0' + days : days
       document.querySelector(".months").innerHTML = month < 10 ? '0' + month : month
-
+}
+else
+{
+      document.querySelector(".seconds").innerHTML = '0';
+      document.querySelector(".minutes").innerHTML = '0';
+      document.querySelector(".hours").innerHTML = '0';
+      document.querySelector(".days").innerHTML = '0';
+      document.querySelector(".months").innerHTML = '0';
+}
 if(diff === 0){
   clearInterval(countdown)
         document.querySelector(".countdown").innerHTML = 'Happy Birthday Ahmed'
